470eace07d7b2526352e65b1a3a9523dc9cfeaf6
aldarion-jdr <68708803+aldarion-jdr@users.noreply.github.com>
2026-01-14 01:45:44 +0100

    Update babele-register.js compatibilité v13
    Premier test pour la v13

--- babele-register.js
+++ babele-register.js
@@ -1,10 +1,8 @@
 import { ActorSheetSFRPGStarship } from "/systems/sfrpg/module/actor/sheet/starship.js";
-Hooks.once("init", () => {
-	if(typeof Babele !== "undefined") {
-		Babele.get().register({
-			module: "sfrpg-fr-translation",
-			lang: "fr",
-			dir: "compendium"
-		});
-	}
+Hooks.once("ready", () => {
+	Babele.get().register({
+		module: "sfrpg-fr-translation",
+		lang: "fr",
+		dir: "compendium"
+	});
 });
